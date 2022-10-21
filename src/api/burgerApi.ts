254d645250c6f86fe34a8c6194da50ec42c69593
Максим Сергеев <msergeev@ibs.ru>
2022-10-21 16:38:04 +0300

    [CB-9] fix: ревью правки

--- src/api/burgerApi.ts
+++ src/api/burgerApi.ts
@@ -2,18 +2,38 @@ import {_baseURL} from "./constants";
 import {checkReponse} from "../utils/functions";
 import {IDataOrderPost} from "../utils/interfaces";
 
+interface IRequest {
+    url: string;
+    options?: {
+        method: string;
+        headers: {
+            [key: string]: string;
+        }
+        body: string;
+    }
+}
+
+export function request({url, options}: IRequest){
+    return fetch(`${url}`, options)
+        .then(res => checkReponse(res));
+}
+
 export function getIngredients() {
-    return fetch(`${_baseURL}/ingredients`)
-        .then(res => checkReponse(res))
+    const url = `${_baseURL}/ingredients`
+    return request({
+        url: url,
+    });
 }
 
 export function postOrder(ingredients: IDataOrderPost) {
-    return fetch(`${_baseURL}/orders`, {
+    const url = `${_baseURL}/orders`;
+    const options =  {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json'
         },
         body: JSON.stringify(ingredients)
-    }).then(res => checkReponse(res))
+    }
+    return request({url, options})
 }
